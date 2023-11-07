c061f5a3a122962030ea9f2ba29b0e2397a30444
Marco Campos <marco.campos24@utexas.edu>
2023-11-07 00:51:43 -0600

    moved question generators to files, added a bunch of functions for factoring questions into the Practice Exams

--- src/components/utils/generateQuestions.js
+++ src/components/utils/generateQuestions.js
@@ -6,8 +6,8 @@ export function generateQuestionsWithSolutions(numberOfQuestions) {
     const generators1a = [foil2x2, foil2x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159, foil3x3]; // Array of functions to generate questions
     const generators1b = [factor1a, factor1b, commonfactorpm, sumconsecutivesquares, differenceSquares]
 
-    const generators = [factor1a]
-    // const generators = [...generators1a, ...generators1b];
+    //const generators = [factor1a]
+    const generators = [...generators1a, ...generators1b];
     for (let i = 0; i < numberOfQuestions; i++) {
       // Randomly select a question generation function and push its output
       let generateQuestion = generators[Math.floor(Math.random() * generators.length)];
