57ff6fa9ddc8ded2ac5dbe0464b3f4c01a207f79
Marco Campos <marco.campos24@utexas.edu>
2023-11-06 23:39:57 -0600

    small bug fix

--- src/components/utils/generateQuestions.js
+++ src/components/utils/generateQuestions.js
@@ -210,8 +210,8 @@ export function squares4159() {
   
 export function generateQuestionsWithSolutions(numberOfQuestions) {
     let questionsWithSolutions = [];
-    const generators = [foil3x3]
-    // const generators = [foil2x2, foil2x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159, foil3x3]; // Array of functions to generate questions
+    //const generators = [foil3x3]
+    const generators = [foil2x2, foil2x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159, foil3x3]; // Array of functions to generate questions
     console.log(numberOfQuestions)
     for (let i = 0; i < numberOfQuestions; i++) {
       // Randomly select a question generation function and push its output
