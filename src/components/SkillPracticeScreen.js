4e760068c94b708f0ae74d41c298ca2567be0d1e
Marco Campos <marco.campos24@utexas.edu>
2023-11-06 23:58:48 -0600

    fixed a bug in the Practice Skills section

--- src/components/SkillPracticeScreen.js
+++ src/components/SkillPracticeScreen.js
@@ -1,7 +1,6 @@
 // components/SkillPracticeScreen.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import TextDisplay from './TextDisplay';
-// Assuming you have functions for each skill type like this:
 import { foil2x2, foil2x3, foil3x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159 } from './utils/generateQuestions';
 
 const SkillPracticeScreen = ({ endPractice, skillType }) => {
@@ -9,76 +8,42 @@ const SkillPracticeScreen = ({ endPractice, skillType }) => {
   const [userInput, setUserInput] = useState('');
   const [score, setScore] = useState(0);
   const [answers, setAnswers] = useState([]);
-console.log(skillType)
-  // This function decides which generator function to use based on the skillType
-  const generateQuestion = () => {
+
+  // Using useCallback to memoize the function so it doesn't change unless skillType changes
+  const generateQuestion = useCallback(() => {
     switch (skillType) {
       case 'foil2x2':
         return foil2x2();
       case 'foil2x3':
         return foil2x3();
-
       case 'foil3x3':
-          return foil3x3()
+        return foil3x3();
       case 'multiply11':
-          return multiply11()
+        return multiply11();
       case 'multiply111':
-          return multiply111()
+        return multiply111();
       case 'multiply101':
-          return multiply101()
+        return multiply101();
       case 'multiply25':
-          return multiply25()
+        return multiply25();
       case 'multiply75': 
-          return multiply75()
+        return multiply75();
       case 'multiplyNear100':
-          return multiplyNear100()
+        return multiplyNear100();
       case 'squaresEnd5':
-          return squaresEnd5()
+        return squaresEnd5();
       case 'squares4159':
-          return squares4159()
-
-      // add more cases for other skills
+        return squares4159();
+      // Add more cases for other skills
       default:
         throw new Error(`Unknown skill type: ${skillType}`);
     }
-  };
+  }, [skillType]);
 
   // Call generateQuestion on component mount and whenever skillType changes
   useEffect(() => {
-    console.log('Skill Type:', skillType)
-    const generateQuestion = () => {
-        switch (skillType) {
-            case 'foil2x2':
-                return foil2x2();
-              case 'foil2x3':
-                return foil2x3();
-        
-              case 'foil3x3':
-                  return foil3x3()
-              case 'multiply11':
-                  return multiply11()
-              case 'multiply111':
-                  return multiply111()
-              case 'multiply101':
-                  return multiply101()
-              case 'multiply25':
-                  return multiply25()
-              case 'multiply75': 
-                  return multiply75()
-              case 'multiplyNear100':
-                  return multiplyNear100()
-              case 'squaresEnd5':
-                  return squaresEnd5()
-              case 'squares4159':
-                  return squares4159()
-          // add more cases for other skills
-          default:
-            throw new Error(`Unknown skill type: ${skillType}`);
-        }
-      };
-    const newQuestion = generateQuestion();
-    setCurrentQuestion(newQuestion);
-  }, [skillType]);
+    setCurrentQuestion(generateQuestion());
+  }, [generateQuestion]);
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
@@ -87,12 +52,24 @@ console.log(skillType)
   const handleSubmit = (e) => {
     e.preventDefault();
     const correct = userInput.trim() === currentQuestion.solution;
-    const updatedScore = correct ? score + 1 : score; // You can adjust scoring as needed
-    setAnswers([...answers, { ...currentQuestion, userInput: userInput.trim(), correct }]);
+    const updatedScore = correct ? score + 1 : score;
+
+    // Update this line to include 'matched' property
+    setAnswers([
+      ...answers,
+      {
+        ...currentQuestion,
+        userInput: userInput.trim(),
+        correct, // This can be kept if you are using it elsewhere
+        matched: correct // Add this line so EndScreen can use it to display colors
+      }
+    ]);
+
     setScore(updatedScore);
     setUserInput('');
     setCurrentQuestion(generateQuestion()); // Generate the next question
-  };
+};
+
 
   // Make sure to render something different if there's no current question
   if (!currentQuestion) {
