dde4c29906a55014e8c891cf5b9a4f5b9ca0090a
Marco Campos <marco.campos24@utexas.edu>
2023-11-09 01:35:54 -0600

    Wrote up some basic stuff in Home, About and Learning

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for internal navigation
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the starting point of our application.</p>
-      {/* Add more content and styling as needed */}
+      <h1>Welcome to the Number Sense Trainer </h1>
+      <div>
+          <p>This (name pending) webapp was built to train the next generation for mental math competitions. No more do we have to rely on the education system bringing us coaches or having to dig for new exams, 
+          my goal is to give you a product that lets test your skills endlessly.</p>
+      </div>
+      <div>
+        <h2>Practice</h2>
+        <p>Ready to test your skills? Head over to the practice section:</p>
+        <Link to="/practice">Train!</Link>
+      </div>
     </div>
   );
 };
--- src/components/LearningPage.js
+++ src/components/LearningPage.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for internal navigation
 
 const LearningPage = () => {
   return (
     <div>
       <h1>Learning Resources</h1>
-      <p>Here you can find various resources to help you learn.</p>
-      {/* Add more content such as articles, videos, and links to learning materials */}
+      
+      {/* Section with external link */}
+      <div>
+        <h2>External Resources</h2>
+        <h3>Books</h3>
+        <p>Bryant Heath wrote a wonderful book several years ago about number sense tricks:</p>
+        <a href="https://bryantheath.com/files/2018/04/Heath_NSTricks_revA.pdf" target="_blank" rel="noopener noreferrer">
+          BryantHeath.com
+        </a>
+        <h3>Texas Math Mundo</h3>
+        <p>This channel was created by a former colleague and rival of mine to bring the competitive math scene in Texas together. Check him out!</p>
+        <a href="https://www.youtube.com/channel/UCDHQCWfSMjNJJS0aJx8jITw" target="_blank" rel="noopener noreferrer">
+          Texas Math Mundo Youtube Channel
+        </a>
+      </div>
+      
+      {/* Section with link to Practice page */}
+      <div>
+        <h2>Practice Skills</h2>
+        <p>Ready to test your skills? Head over to the practice section:</p>
+        <Link to="/practice">Go to Practice</Link>
+      </div>
     </div>
   );
 };
