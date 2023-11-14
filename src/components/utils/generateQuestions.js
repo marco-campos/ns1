4f5b10cdd3eecb1ea2069c6732e15eefb9d7b1f7
Marco Campos <marco.campos24@utexas.edu>
2023-11-14 01:24:37 -0600

    added new problems

--- src/components/utils/generateQuestions.js
+++ src/components/utils/generateQuestions.js
@@ -1,10 +1,10 @@
-import { foil2x2, foil2x3, foil3x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159 } from './section1/section1a';
-import { factor1a, factor1b, commonfactorpm, sumconsecutivesquares, differenceSquares} from './section1/section1b';  
-import { mixedFoil1, axaOverb, a_bPlusb_a, a_bMinusNfrac1, a_bMinusNfrac2} from './section1/section1c';
-import { remainderBy11, remainderBy3or9, remainderBy6or12or18, remainderBy2or4or8, complexRemainderQuestion } from './section1/section1d';
-import { squares, squareRoot, cubes, cubeRoot} from './section2/section2a'
-import {squaresdecimal, squaresPercentage, romanNumeral} from './section2/section2b'
-import {firstmInts,firstmOdds, firstmEvens,firstmSquares,firstmCubes,arithmeticSeries} from './section2/section2c'
+// import { foil2x2, foil2x3, foil3x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159 } from './section1/section1a';
+// import { factor1a, factor1b, commonfactorpm, sumconsecutivesquares, differenceSquares} from './section1/section1b';  
+// import { mixedFoil1, axaOverb, a_bPlusb_a, a_bMinusNfrac1, a_bMinusNfrac2} from './section1/section1c';
+// import { remainderBy11, remainderBy3or9, remainderBy6or12or18, remainderBy2or4or8, complexRemainderQuestion } from './section1/section1d';
+// import { squares, squareRoot, cubes, cubeRoot} from './section2/section2a'
+// import {squaresdecimal, squaresPercentage, romanNumeral} from './section2/section2b'
+// import {firstmInts,firstmOdds, firstmEvens,firstmSquares,firstmCubes,arithmeticSeries} from './section2/section2c'
   //infGeoSeries} from './section2/section2c'
 
 // Move these up later.
@@ -23,21 +23,36 @@ import { generator4a } from './section4/section4a';
 
 export function generateQuestionsWithSolutions(numberOfQuestions) {
   let questionsWithSolutions = [];
-  const generators1a = [foil2x2, foil2x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159, foil3x3]; // Array of functions to generate questions
-  const generators1b = [factor1a, factor1b, commonfactorpm, sumconsecutivesquares, differenceSquares]
-  const generators1c = [mixedFoil1, axaOverb, a_bPlusb_a, a_bMinusNfrac1, a_bMinusNfrac2]
-  const generators1d = [remainderBy3or9, remainderBy11, remainderBy6or12or18, remainderBy2or4or8, complexRemainderQuestion]
+  // const generators1a = [foil2x2, foil2x3, multiply11, multiply111, multiply101, multiply25, multiply75, multiplyNear100, squaresEnd5, squares4159, foil3x3]; // Array of functions to generate questions
+  // const generators1b = [factor1a, factor1b, commonfactorpm, sumconsecutivesquares, differenceSquares]
+  // const generators1c = [mixedFoil1, axaOverb, a_bPlusb_a, a_bMinusNfrac1, a_bMinusNfrac2]
+  // const generators1d = [remainderBy3or9, remainderBy11, remainderBy6or12or18, remainderBy2or4or8, complexRemainderQuestion]
   
-  const generators2a = [squares, squareRoot, cubes, cubeRoot]
-  const generators2b = [squaresdecimal, squaresPercentage, romanNumeral]
-  const generators2c = [firstmInts,firstmOdds, firstmEvens,firstmSquares,firstmCubes,arithmeticSeries]
+  // const generators2a = [squares, squareRoot, cubes, cubeRoot]
+  // const generators2b = [squaresdecimal, squaresPercentage, romanNumeral]
+  // const generators2c = [firstmInts,firstmOdds, firstmEvens,firstmSquares,firstmCubes,arithmeticSeries]
     //infGeoSeries]
+
+  const generators1a =Object.values(generator1a)
+  const generators1b =Object.values(generator1b)
+  const generators1c =Object.values(generator1c)
+  const generators1d =Object.values(generator1d)
+  const generators2a =Object.values(generator2a)
+  const generators2b =Object.values(generator2b)
+  const generators2c =Object.values(generator2c)
+  const generators3a =Object.values(generator3a)
+  const generators3b =Object.values(generator3b)
+  const generators3c =Object.values(generator3c)
+  const generators4a =Object.values(generator4a)
   
   //const generators = [axaOverb]
 
   const generators1 = [...generators1a, ...generators1b, ...generators1c, ...generators1d];
   const generators2 = [...generators2a, ...generators2b, ...generators2c]
-  const generators = [...generators1, ...generators2]
+  const generators3 = [...generators3a, ...generators3b, ...generators3c]
+  const generators4 = [...generators4a]
+
+  const generators = [...generators1, ...generators2, ...generators3, ...generators4]
     for (let i = 0; i < numberOfQuestions; i++) {
       // Randomly select a question generation function and push its output
       let generateQuestion = generators[Math.floor(Math.random() * generators.length)];
