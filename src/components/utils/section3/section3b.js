d31c5de817b786ca41ee3e7614086daa351745ff
Marco Campos <marco.campos24@utexas.edu>
2023-11-13 23:24:35 -0600

    more s3

--- src/components/utils/section3/section3b.js
+++ src/components/utils/section3/section3b.js
@@ -33,12 +33,86 @@ export function primeIntegralDivisors() {
     
     return pair;
 }
+
+export function numOfIntegralDivisors() {
+    const primes = [2, 3, 5, 7, 11];
+    const primeFactors = [];
+    const exponents = [];
+    const numPrimes = Math.floor(Math.random() * 2) + 3; // 2 or 3 prime factors
+
+    for (let i = 0; i < numPrimes; i++) {
+        let prime = primes[Math.floor(Math.random() * primes.length)];
+        while (primeFactors.includes(prime)){
+            prime = primes[Math.floor(Math.random() * primes.length)]
+        }
+        const exponent = Math.floor(Math.random() * 5) + 1; // exponent between 1 and 5
+        primeFactors.push(prime);
+        exponents.push(exponent);
+    }
+
+    let number 
+    if (numPrimes === 3){
+        number = `${primeFactors[0]}^${exponents[0]} \\times ${primeFactors[1]}^${exponents[1]}  \\times ${primeFactors[2]}^${exponents[2]}`
+    } else{
+        number = `${primeFactors[0]}^${exponents[0]} \\times ${primeFactors[1]}^${exponents[1]}  \\times ${primeFactors[2]}^${exponents[2]} \\times ${primeFactors[3]}^${exponents[3]}`
+    }
+    const question = `\\text{ Find the number of integral divisors of }${number}.`;
+    const answer = String(exponents.reduce((acc, e) => acc * (e + 1), 1));
+    const pair = {
+        question: question,
+        solution: answer,
+    };
+
+    return pair;
+}
+
+export function sumOfIntegralDivisors() {
+    const primes = [2, 3, 5];
+    const primeFactors = [];
+    const exponents = [];
+    const numPrimes = 2; // 2 or 3 prime factors
+
+    for (let i = 0; i < numPrimes; i++) {
+        let prime = primes[Math.floor(Math.random() * primes.length)];
+        while (primeFactors.includes(prime)){
+            prime = primes[Math.floor(Math.random() * primes.length)]
+        }
+        const exponent = Math.floor(Math.random() * 2) + 1; // exponent between 1 and 5
+        primeFactors.push(prime);
+        exponents.push(exponent);
+    }
+
+    let sum = 1; // Start with 1 because we'll be multiplying terms.
+    console.log(primeFactors, exponents)
+    for (let i = 0; i < primeFactors.length; i++) {
+        // Calculate each term of the product using the formula
+        let term = (Math.pow(primeFactors[i], exponents[i] + 1) - 1) / (primes[i] - 1);
+        sum *= term;
+    }
+    const number = primeFactors.reduce((acc, prime, idx) => acc * Math.pow(prime, exponents[idx]), 1);
+
+    const question = `\\text{Find the sum of integral divisors of }${number}.`;
+    const answer = String(sum);
+    const pair = {
+        question: question,
+        solution: answer,
+    };
+
+    return pair;
+}
+
+
+
+
 export const generator3b = {
-    'primeIntegralDivisors': primeIntegralDivisors
+    'primeIntegralDivisors': primeIntegralDivisors,
+    'numOfIntegralDivisors':numOfIntegralDivisors,
+    'sumOfIntegralDivisors':sumOfIntegralDivisors
 }
 
 export const s3bSkills = [
     { value: 'primeIntegralDivisors', label: 'Number of Prime Integral Divisors' },
-    
+    { value: 'numOfIntegralDivisors', label: 'Number of Integral Divisors' },
+    { value: 'sumOfIntegralDivisors', label: 'Sum of Integral Divisors' },
   ];
   
